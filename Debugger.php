dfcff81cb26204b77b418964841de66611f3fa62
Ivan Vegner <vegner@slrealty.ru>
2017-12-20 12:40:57 +0300

    New front description

--- Debugger.php
+++ Debugger.php
@@ -25,7 +25,7 @@ class Debugger {
         $this->isDebug = $isDebug;
         if ($isDebug) {
             ini_set('display_errors', 1);
-            error_reporting(E_ALL);
+            error_reporting(E_ALL & ~E_DEPRECATED);
         }
     }
 
